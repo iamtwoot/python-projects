f64bb4158244dee50cc948ab98fb94c9361ccec7
Twoot <inenquser@gmail.com>
2026-03-28 15:30:17 +0300

    create quiz brain class with next question method

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -1,5 +1,8 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = [Question(question["text"], question["answer"]) for question in question_data]
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
