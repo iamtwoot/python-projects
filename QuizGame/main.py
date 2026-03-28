c76d0826d655a624e776faad43b583b4b3222f22
Twoot <inenquser@gmail.com>
2026-03-28 16:08:58 +0300

    add off command to end the quiz

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -9,4 +9,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You`ve completed the quiz.")
-print(f"Your final score was: {quiz.score}/{len(question_bank)}")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
