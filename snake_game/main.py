2c40c2825db64aadfe6f5bcc76e71cf8756227fa
Twoot <inenquser@gmail.com>
2026-03-30 17:04:03 +0300

    Create move method for snake class

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,6 +2,8 @@ from turtle import Screen
 import time
 from snake import Snake
 
+MOVE_DISTANCE = 20
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -14,7 +16,7 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    snake.move()
+    snake.move(MOVE_DISTANCE)
 
 
 
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -4,7 +4,9 @@ class Snake:
 
     def __init__(self):
         self.segments = []
+        self.create_snake()
 
+    def create_snake(self):
         start_x = 0
         for _ in range(3):
             new_segment = Turtle(shape="square")
@@ -14,10 +16,9 @@ class Snake:
             start_x -= 20
             self.segments.append(new_segment)
 
-    def move(self):
+    def move(self, distance):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
-        self.segments[0].left(90)
+        self.segments[0].forward(distance)
