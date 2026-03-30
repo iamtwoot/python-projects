c6126865434522c7d490cfcc16e69dd9f0c1a643
Twoot <inenquser@gmail.com>
2026-03-30 17:18:46 +0300

    Add key listen logic for controlling the snake

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,7 +2,6 @@ from turtle import Screen
 import time
 from snake import Snake
 
-MOVE_DISTANCE = 20
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,11 +11,17 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    snake.move(MOVE_DISTANCE)
+    snake.move()
 
 
 
