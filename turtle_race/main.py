fb5bcc495c30b03fdc5275507fb597572e3bc86f
Twoot <inenquser@gmail.com>
2026-03-30 15:24:10 +0300

    Add competition logic for turtles

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -1,18 +1,39 @@
+import random
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
+
+is_race_on = False
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-
+all_turtles = []
 start_y = -70
 
 for turtle_index in range(6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(-230, start_y)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(-230, start_y)
+    all_turtles.append(new_turtle)
     start_y += 30
 
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You`ve won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You`ve lost! The {winning_color} turtle is the winner!")
+
+
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
