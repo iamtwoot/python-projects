03ecabd6155734b24654ef4c6b2536722dac8138
Twoot <inenquser@gmail.com>
2026-03-31 14:20:25 +0300

    Separated bounce_x method to fix the bag when ball hits paddle in the corner

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -9,6 +9,7 @@ class Ball(Turtle):
         self.penup()
         self.y_step = 10
         self.x_step = 10
+        self.move_speed = 0.1
 
     def move(self):
         self.goto(self.xcor() + self.x_step, self.ycor() + self.y_step)
@@ -16,10 +17,16 @@ class Ball(Turtle):
     def bounce_y(self):
         self.y_step *= -1
 
-    def bounce_x(self):
-        self.x_step *= -1
+    def bounce_x_r_paddle(self):
+        self.x_step = -(abs(self.x_step))
+        self.move_speed *= 0.9
+
+    def bounce_x_l_paddle(self):
+        self.x_step = abs(self.x_step)
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0,0)
-        self.bounce_x()
+        self.move_speed = 0.1
+        self.x_step *= -1
         self.move()
--- pong_game/main.py
+++ pong_game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(r_paddle.up, "Up")
@@ -22,7 +24,7 @@ screen.onkeypress(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     screen.update()
 
@@ -30,15 +32,23 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with paddles
-    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320 or
-            ball.distance(l_paddle) < 50 and ball.xcor() < -320):
-        ball.bounce_x()
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 63 and ball.xcor() > 320:
+        ball.bounce_x_r_paddle()
 
-    # Detect if the ball goes past the paddle
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    # Detect collision with left paddle
+    if ball.distance(l_paddle) < 63 and ball.xcor() < -320:
+        ball.bounce_x_l_paddle()
+
+    # Detect R paddle misses
+    if ball.xcor() > 370:
         ball.reset_position()
+        scoreboard.l_point()
 
+    # Detect L paddle misses
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 
