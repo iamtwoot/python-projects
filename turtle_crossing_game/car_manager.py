7cba7365ba5d18aec39b6ea659f58794110482d2
Twoot <inenquser@gmail.com>
2026-03-31 15:58:52 +0300

    Create car manager and cars appearing logic

--- turtle_crossing_game/car_manager.py
+++ turtle_crossing_game/car_manager.py
@@ -1,7 +1,24 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self, y, x=350):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_len=2, stretch_wid=1)
+        self.setheading(180)
+        self.color(random.choice(COLORS))
+        self.goto(x, y)
+
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
+
+    def off_screen(self):
+        return self.xcor() < -340
--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from turtle import Screen
 from player import Player
@@ -9,6 +10,8 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player_object = Player()
+cars = [CarManager(x=random.randint(-250, 280), y=random.randint(-230, 280)) for _ in range(25)]
+
 
 screen.listen()
 screen.onkeypress(player_object.up, "Up")
@@ -17,3 +20,9 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    for car in cars:
+        car.move()
+        if car.off_screen():
+            cars.remove(car)
+            cars.append(CarManager(y=random.randint(-250, 280)))
