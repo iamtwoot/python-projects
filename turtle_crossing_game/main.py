d9d163fe83c3a1f81526ca1c97a55d1d08f8fdcd
Twoot <inenquser@gmail.com>
2026-03-31 15:18:51 +0300

    Create player initialization

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player_object = Player()
+
+screen.listen()
+screen.onkeypress(player_object.up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
