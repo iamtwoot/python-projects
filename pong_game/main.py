d2fc2c8d35110ed0b80d332f98b16ba0be379123
Twoot <inenquser@gmail.com>
2026-03-31 11:54:59 +0300

    Create paddle class

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,13 +1,23 @@
 from turtle import Screen
+from paddle import Paddle
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
+paddle = Paddle()
+paddle.goto(350,0)
 
+screen.listen()
+screen.onkeypress(paddle.up, "Up")
+screen.onkeypress(paddle.down, "Down")
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
