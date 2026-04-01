0e6280bef02312bb55cba4c9f82999a7d7503437
Twoot <inenquser@gmail.com>
2026-04-01 09:35:23 +0300

    Add high score and snake reset logic

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+from main import segment
+
 START_X = 0
 MOVE_DISTANCE = 20
 UP = 90
@@ -20,13 +22,19 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
         new_segment = Turtle(shape="square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
