76b648075bbc5c9671ab80ac50b530e52b896118
Twoot <inenquser@gmail.com>
2026-04-02 15:49:13 +0300

    Create main window

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,20 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=img)
+canvas.grid(row=0, column=0)
+
+
+
+window.mainloop()
