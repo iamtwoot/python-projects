3f45037a50e980e04a23a291e3fd89a98b3a02a7
Twoot <inenquser@gmail.com>
2026-04-02 16:03:53 +0300

    Create widgets on screen

--- password_manager/main.py
+++ password_manager/main.py
@@ -12,9 +12,35 @@ window.config(padx=20, pady=20)
 
 img = PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200)
-canvas.create_image(100, 100, image=img)
-canvas.grid(row=0, column=0)
+canvas.create_image(130, 100, image=img)
+canvas.grid(row=0, column=1)
 
+# Row 1
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
 
+website_entry = Entry()
+website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
+
+# Row 2
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry()
+email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
+
+# Row 3
+password_label = Label(text="Password")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry()
+password_entry.grid(row=3, column=1, sticky="ew")
+
+password_button = Button(text="Generate Password")
+password_button.grid(row=3, column=2)
+
+# Row 4
+add_button = Button(text="Add")
+add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
 
 window.mainloop()
