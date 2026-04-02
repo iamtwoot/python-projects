9613d7457d29729ec8e0b11c0fa8b77e8af7a598
Twoot <inenquser@gmail.com>
2026-04-02 12:29:48 +0300

    Setup UI and create countdown mechanism

---
+++ pomodoro_timer/main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+CHECKMARK = "✔"
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+def start_timer():
+    count_down(5)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.grid(row=1, column=1)
+
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+start_button = Button(text="start", command=start_timer)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="reset")
+reset_button.grid(row=2, column=3)
+
+checkmark = Label(text=CHECKMARK,fg=GREEN, bg=YELLOW)
+checkmark.grid(row=3, column=1)
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
