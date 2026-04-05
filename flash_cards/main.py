dd7decdb6e813b3d49e28c1586732e4fd531e113
Twoot <inenquser@gmail.com>
2026-04-05 12:23:36 +0300

    Create new class for UI and App logic

--- flash_cards/main.py
+++ flash_cards/main.py
@@ -1,42 +1,47 @@
-import random
 from tkinter import *
-import pandas as pd
+from cards import Cards
 
 BACKGROUND_COLOR = "#B1DDC6"
 MAIN_FONT = ("Ariel", 40, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
 
+class FlashcardApp:
 
-# -------------------------- SETUP UI --------------------------------- #
-window = Tk()
-window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
-window.title("Flashy")
+    def __init__(self):
+        self.cards = Cards()
+
+        self.window = Tk()
+        self.window.title("Flashy")
+        self.window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+        self.card_back_img = PhotoImage(file="images/card_back.png")
+        self.card_front_img = PhotoImage(file="images/card_front.png")
+        self.wrong_img = PhotoImage(file="images/wrong.png")
+        self.right_img = PhotoImage(file="images/right.png")
+
+        self.canvas = Canvas(
+            width=800,
+            height=526,
+            highlightthickness=0,
+            bg=BACKGROUND_COLOR
+        )
+        self.bg_image = self.canvas.create_image(400, 263, image=self.card_front_img)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
+        self.card_title = self.canvas.create_text(400, 150, text="", font=MAIN_FONT)
+        self.card_word = self.canvas.create_text(400, 263, text="", font=WORD_FONT)
+
+        # Buttons
+        wrong_button = Button(image=self.wrong_img, highlightthickness=0,command=self.cards.next_card)
+        wrong_button.grid(row=1, column=0)
+        wrong_button.config(borderwidth=0)
+
+        right_button = Button(image=self.right_img, highlightthickness=0, command=self.cards.mark_known)
+        right_button.grid(row=1, column=1)
+        right_button.config(borderwidth=0)
+
+        self.window.mainloop()
 
 
-# Images
-card_back_img = PhotoImage(file="images/card_back.png")
-card_front_img = PhotoImage(file="images/card_front.png")
-wrong_img = PhotoImage(file="images/wrong.png")
-right_img = PhotoImage(file="images/right.png")
-
-# Card
-canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-bg_image = canvas.create_image(400, 263, image=card_front_img)
-canvas.grid(row=0, column=0, columnspan=2)
-
-card_title = canvas.create_text(400, 150, text="", font=MAIN_FONT)
-card_word = canvas.create_text(400, 263, text="", font=WORD_FONT)
-
-# Buttons
-wrong_button = Button(image=wrong_img, highlightthickness=0, command=card.mark_unknown)
-wrong_button.grid(row=1, column=0)
-wrong_button.config(borderwidth=0)
-
-right_button = Button(image=right_img, highlightthickness=0, command=card.mark_known)
-right_button.grid(row=1, column=1)
-right_button.config(borderwidth=0)
-
-
-window.mainloop()
 
 
