fe9d5b8372afd4cbe443720e5a4c8617cb6ef992
Twoot <inenquser@gmail.com>
2026-04-06 10:32:01 +0300

    Initial commit

---
+++ birthday_wisher/main.py
@@ -0,0 +1,15 @@
+##################### Extra Hard Starting Project ######################
+import os
+import random
+import pandas as pd
+import datetime as dt
+
+
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
