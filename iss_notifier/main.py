5f7eae0ca962428ae6b14c94d403d3340b7466d7
Twoot <inenquser@gmail.com>
2026-04-06 18:03:10 +0300

    Create tracking ISS position and sending email logic

--- iss_notifier/main.py
+++ iss_notifier/main.py
@@ -1,8 +1,21 @@
+import smtplib
+import time
 import requests
 from datetime import datetime
+from zoneinfo import ZoneInfo
+import os
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+def get_env(name):
+    value = os.getenv(name)
+    if value is None:
+        raise ValueError(f"Environment variable {name} not set.")
+    return value
+
+MY_EMAIL = get_env("MY_EMAIL")
+MY_PASSWORD = get_env("MY_PASSWORD")
+
+MY_LAT = 56.376859 # Your latitude
+MY_LONG = 44.042414 # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -12,7 +25,11 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
-
+def is_position_within(iss_lat, iss_long):
+    if ((MY_LAT - 5 < iss_lat < MY_LAT + 5) and
+            (MY_LONG - 5 < iss_long < MY_LONG + 5)):
+        return True
+    return False
 
 parameters = {
     "lat": MY_LAT,
@@ -26,12 +43,17 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+time_now = datetime.now(ZoneInfo("Europe/Moscow")).hour
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
-
-
+while True:
+    if is_position_within(iss_latitude, iss_longitude):
+        if sunset < time_now < sunrise:
+            with smtplib.SMTP('smtp.gmail.com') as connection:
+                connection.starttls()
+                connection.login(MY_EMAIL, MY_PASSWORD)
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=MY_EMAIL,
+                    msg=f"Subject: Look up!\n\nLook up! ISS is currently flying above you.",
+                )
+    time.sleep(60)
