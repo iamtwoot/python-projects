0a30429d7963231b94c74d6f8ad88e3e8d5525db
Twoot <inenquser@gmail.com>
2026-04-07 11:26:27 +0300

    Create UI

--- quiz_game/ui.py
+++ quiz_game/ui.py
@@ -1,2 +1,36 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+FONT = ("Arial", 20, "italic")
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(bg="white", width=300, height=250)
+        self.q_text = self.canvas.create_text(
+            150,
+            125,
+            text="Test",
+            fill=THEME_COLOR,
+            font=FONT,
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        false_img = PhotoImage(file="images/false.png")
+
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
