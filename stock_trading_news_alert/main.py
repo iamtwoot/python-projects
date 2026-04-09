3c843121b096405f5d8d4e380193db875866b1a1
Twoot <inenquser@gmail.com>
2026-04-09 16:54:49 +0300

    Receives stock data from API and calculates price diff

--- stock_trading_news_alert/main.py
+++ stock_trading_news_alert/main.py
@@ -1,8 +1,38 @@
+import requests
+from datetime import date, timedelta
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+ALPHA_API_KEY = "AM4CYEHS3I1KGJ8H"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+alpha_url = "https://www.alphavantage.co/query?"
+alpha_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": ALPHA_API_KEY,
+}
+
+response = requests.get(alpha_url, params=alpha_params)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+
+yesterday = date.today() - timedelta(days=1)
+yesterday_str = yesterday.strftime("%Y-%m-%d")
+
+day_before_yesterday = date.today() - timedelta(days=2)
+day_before_yesterday_str = day_before_yesterday.strftime("%Y-%m-%d")
+
+yesterday_close = float(data[yesterday_str]["4. close"])
+day_before_yesterday_close = float(data[day_before_yesterday_str]["4. close"])
+
+percent_price_diff = round((yesterday_close - day_before_yesterday_close) / day_before_yesterday_close * 100, 2)
+print(percent_price_diff)
+
+if abs(percent_price_diff) > 5:
+    print("Get News")
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
