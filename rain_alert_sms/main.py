20fd57046b7c232266cc18d3813c9db38069ad80
Twoot <inenquser@gmail.com>
2026-04-09 16:12:37 +0300

    Check for bad weather for next day and send sms

--- rain_alert_sms/main.py
+++ rain_alert_sms/main.py
@@ -13,7 +13,21 @@ params = {
 
 url = f"https://api.openweathermap.org/data/2.5/forecast"
 
+def send_sms():
+    print("Sending message...")
+    print("It will rain today.")
+
 response = requests.get(url, params=params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+
+weather_id_list = []
+will_rain = False
+for three_hour_data in weather_data["list"]:
+    weather_id = three_hour_data["weather"][0]["id"]
+    if int(weather_id) < 700:
+        will_rain = True
+
+if will_rain:
+    send_sms()
+
