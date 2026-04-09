22bc0ad9c3f5babd3848ccf3a383390573955036
Twoot <inenquser@gmail.com>
2026-04-09 17:07:08 +0300

    Get news for company name with an API

--- stock_trading_news_alert/main.py
+++ stock_trading_news_alert/main.py
@@ -3,10 +3,9 @@ from datetime import date, timedelta
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-ALPHA_API_KEY = "AM4CYEHS3I1KGJ8H"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# Alpha (stocks) data
+ALPHA_API_KEY = "AM4CYEHS3I1KGJ8H"
 alpha_url = "https://www.alphavantage.co/query?"
 alpha_params = {
     "function": "TIME_SERIES_DAILY",
@@ -14,28 +13,47 @@ alpha_params = {
     "apikey": ALPHA_API_KEY,
 }
 
-response = requests.get(alpha_url, params=alpha_params)
-response.raise_for_status()
-data = response.json()["Time Series (Daily)"]
-
-yesterday = date.today() - timedelta(days=1)
-yesterday_str = yesterday.strftime("%Y-%m-%d")
-
-day_before_yesterday = date.today() - timedelta(days=2)
-day_before_yesterday_str = day_before_yesterday.strftime("%Y-%m-%d")
-
-yesterday_close = float(data[yesterday_str]["4. close"])
-day_before_yesterday_close = float(data[day_before_yesterday_str]["4. close"])
-
-percent_price_diff = round((yesterday_close - day_before_yesterday_close) / day_before_yesterday_close * 100, 2)
-print(percent_price_diff)
-
-if abs(percent_price_diff) > 5:
-    print("Get News")
+# News data
+NEWS_API_KEY = "56cd6bf338af4effbfa2cf3bd7d2afa7"
+news_url = "https://newsapi.org/v2/everything?"
+news_params = {
+    "apiKey": NEWS_API_KEY,
+    "q": COMPANY_NAME,
+    "pageSize": 3,
+}
 
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+def calc_price_diff():
+
+    response = requests.get(alpha_url, params=alpha_params)
+    response.raise_for_status()
+    data = response.json()["Time Series (Daily)"]
+
+    yesterday = date.today() - timedelta(days=1)
+    yesterday_str = yesterday.strftime("%Y-%m-%d")
+
+    day_before_yesterday = date.today() - timedelta(days=2)
+    day_before_yesterday_str = day_before_yesterday.strftime("%Y-%m-%d")
+
+    yesterday_close = float(data[yesterday_str]["4. close"])
+    day_before_yesterday_close = float(data[day_before_yesterday_str]["4. close"])
+
+    percent_price_diff = round((yesterday_close - day_before_yesterday_close) / day_before_yesterday_close * 100, 2)
+    print(percent_price_diff)
+    return percent_price_diff
+
+def get_news():
+    response = requests.get(news_url, params=news_params)
+    response.raise_for_status()
+    data = response.json()["articles"]
+    for news in data:
+        print(news["title"])
+
+
+if abs(calc_price_diff()) > 0:
+    get_news()
+
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
