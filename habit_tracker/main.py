ae47509baf7b6eba5e982e78e0a0ca06a25a2a51
Twoot <inenquser@gmail.com>
2026-04-13 15:10:01 +0300

    Create graph using POST request

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,15 +1,36 @@
 import requests
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+USERNAME = os.getenv("PIXELA_USERNAME")
+TOKEN = os.getenv("TOKEN")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": os.getenv("TOKEN"),
-    "username": os.getenv("USERNAME"),
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Minutes Read Graph",
+    "unit": "minutes",
+    "type": "int",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
