95ad1bac1837c1452621702cf3a20e73cabec356
Twoot <inenquser@gmail.com>
2026-04-13 15:24:50 +0300

    Add pixel using POST request

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,36 +1,49 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+
 load_dotenv()
 
 USERNAME = os.getenv("PIXELA_USERNAME")
 TOKEN = os.getenv("TOKEN")
 
 pixela_endpoint = "https://pixe.la/v1/users"
-
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": "graph1",
-    "name": "Minutes Read Graph",
-    "unit": "minutes",
-    "type": "int",
-    "color": "shibafu",
-}
-
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# CREATE A USER
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes",
+# }
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# CREATE A GRAPH
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# graph_config = {
+#     "id": "graph1",
+#     "name": "Minutes Read Graph",
+#     "unit": "minutes",
+#     "type": "int",
+#     "color": "shibafu",
+# }
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+# ADD A PIXEL
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+
+today = datetime.today()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "60",
+}
+response = requests.post(pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
 
