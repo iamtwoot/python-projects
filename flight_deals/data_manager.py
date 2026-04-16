686c662f530b86a7de8e8e361a40967751c33eae
Twoot <inenquser@gmail.com>
2026-04-16 15:57:13 +0300

    Create get_env_variable() function and initial attributes of DataManager class

--- flight_deals/data_manager.py
+++ flight_deals/data_manager.py
@@ -1,3 +1,26 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_env_variable(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise ValueError(f"Environment variable '{name}' is not set")
+    return value
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.sheety_url = get_env_variable("SHEETY_API")
+        self.sheety_token = get_env_variable("SHEETY_TOKEN")
+
+        self.headers = {"Authorization": f"Bearer {self.sheety_token}"}
+        self.data = None
+
+    def get_data(self):
+        response = requests.get(url=self.sheety_url, headers=self.headers)
+        self.data = response.json()
+        print(self.data)
