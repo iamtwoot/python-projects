c60a0e2055f58d012a0706d3bdf57aa9f2ff344a
Twoot <inenquser@gmail.com>
2026-04-16 16:17:25 +0300

    Add requests_cache for using cache to save API responses locally

--- flight_deals/main.py
+++ flight_deals/main.py
@@ -1,7 +1,7 @@
 from data_manager import DataManager
 import requests_cache
 
-requests_cache.install_cache("flight_cache")
+requests_cache.install_cache("flight_cache", expire_after=3600)
 
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
