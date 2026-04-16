1c6fd4e5faa1ee871ebf6ed2e503309a4b2352d9
Twoot <inenquser@gmail.com>
2026-04-16 16:47:31 +0300

    Cannot use an API for flights due to restrictions on Russian phone numbers

--- flight_deals/main.py
+++ flight_deals/main.py
@@ -1,7 +1,14 @@
 from data_manager import DataManager
 import requests_cache
+from datetime import datetime, timedelta
 
 requests_cache.install_cache("flight_cache", expire_after=3600)
 
-data_manager = DataManager()
-sheet_data = data_manager.get_data()
+# --------------------- Set the Dates --------------------- #
+tomorrow = datetime.today() + timedelta(days=1)
+six_months_from_today = datetime.today() + timedelta(days=180)
+
+# Cannot use an API for flights due to restrictions on Russian phone numbers
+
+# data_manager = DataManager()
+# sheet_data = data_manager.get_data()
