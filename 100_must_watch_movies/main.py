514441e49074549aa090dbc36e965f485431a405
Twoot <inenquser@gmail.com>
2026-04-22 19:21:16 +0300

    Remove the test code

--- 100_must_watch_movies/main.py
+++ 100_must_watch_movies/main.py
@@ -1,12 +1,4 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://news.ycombinator.com/news")
-yc_web_page = response.text
 
-soup = BeautifulSoup(yc_web_page, "html.parser")
-article_tag = soup.select("span.titleline a")
-
-article_text = article_tag[0].text
-article_link = article_tag[0].get("href")
-article_upvote = soup.select("span.score")[0].text
