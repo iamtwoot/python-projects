fd394b47994d745a34619c10fff6a86bd22f26bb
Twoot <inenquser@gmail.com>
2026-04-22 21:13:37 +0300

    Add new file creation logic

--- 100_must_watch_movies/main.py
+++ 100_must_watch_movies/main.py
@@ -5,10 +5,16 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 response = requests.get(URL)
 response.raise_for_status()
+
 html = response.text
+
 soup = BeautifulSoup(html, 'html.parser')
 
 titles = soup.select("div.article-title-description h3.title")
 titles_reverse = titles[::-1]
+
+with open("movies.txt", "w", encoding="UTF-8") as file:
+    for title in titles_reverse:
+        file.write(title.text + "\n")
 
 
