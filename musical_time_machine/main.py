ebeaf152b5bf6399945e3d3af1b70948dae743c4
Twoot <inenquser@gmail.com>
2026-04-23 12:26:57 +0300

    feat: parse song titles from Billboard page
    - extract song names using BeautifulSoup
    - select titles via CSS selector
    - output parsed titles to console

--- musical_time_machine/main.py
+++ musical_time_machine/main.py
@@ -1,10 +1,19 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
-url = "https://appbrewery.github.io/bakeboard-hot-100/"
+load_dotenv()
 
-prompt = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-response = requests.get(f"{url}{prompt}")
+# prompt = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+url = f"https://appbrewery.github.io/bakeboard-hot-100/2026-04-18/"
+
+response = requests.get(url)
 response.raise_for_status()
-html = response.text
+
+soup = BeautifulSoup(response.text, "html.parser")
+song_names = [tag.getText() for tag in soup.select("h3.chart-entry__title")]
