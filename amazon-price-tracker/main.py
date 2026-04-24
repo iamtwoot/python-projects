6fb84790bf72881ef889fed445ccb8dbdea8169a
Twoot <inenquser@gmail.com>
2026-04-24 19:35:39 +0300

    Extract and parse product price using a-offscreen selector

--- amazon-price-tracker/main.py
+++ amazon-price-tracker/main.py
@@ -1,3 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://appbrewery.github.io/instant_pot/"
+
+html = requests.get(url).text
+soup = BeautifulSoup(html, "html.parser")
+
+price_tag = soup.select_one(".a-offscreen")
+price_text = price_tag.get_text()
+price = float(price_text.replace("$", ""))
