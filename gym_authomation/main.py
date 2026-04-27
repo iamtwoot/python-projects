1ffabdef0f54b1e88f41a023309bc8dbf26bf385
Twoot <inenquser@gmail.com>
2026-04-27 16:51:48 +0300

    Sign up test user

--- gym_authomation/main.py
+++ gym_authomation/main.py
@@ -1,13 +1,15 @@
 import os
 from selenium import webdriver
 
-URL = "https://appbrewery.github.io/gym/"
+GYM_URL = "https://appbrewery.github.io/gym/"
+ACCOUNT_EMAIL = "daniil@test.ru"
+ACCOUNT_PASSWORD = "g45yg45g45g34g4efdhgaJHGV"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+driver = webdriver.Chrome(options=chrome_options)
 
-
-driver = webdriver.Chrome()
+driver.get(GYM_URL)
