8f2422468010ad7b9f810b237df4300188f8a68c
Twoot <inenquser@gmail.com>
2026-04-27 14:30:21 +0300

    Add url and open it in chrome

--- game_clicker_bot/main.py
+++ game_clicker_bot/main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 
+URL = "https://ozh.github.io/cookieclicker/"
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(chrome_options)
+driver.get(URL)
+
