918d2c039cd20fe86db49dbf5941826a5993ab8f
Twoot <inenquser@gmail.com>
2026-04-27 15:31:26 +0300

    Add sleep function to wait the cookie to loads

--- game_clicker_bot/main.py
+++ game_clicker_bot/main.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,5 +19,9 @@ language_div = wait.until(
 )
 language_div.click()
 
+time.sleep(5)
 
 cookie = driver.find_element(By.XPATH, '//*[@id="bigCookie"]')
+
+while True:
+    cookie.click()
