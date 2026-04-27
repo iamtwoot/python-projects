7d9d0f49be650b8a3e2fcb900bc26262d8c7e7ec
Twoot <inenquser@gmail.com>
2026-04-27 16:01:44 +0300

    Check for upgrades every 5 seconds and buy the most expensive one

--- game_clicker_bot/main.py
+++ game_clicker_bot/main.py
@@ -5,6 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 URL = "https://ozh.github.io/cookieclicker/"
+CHECK_INTERVAL = 5
+
+def check_for_upgrades():
+    available_upgrades = driver.find_elements(By.CSS_SELECTOR, "#products .enabled")
+    latest_upgrade = available_upgrades[-1]
+    latest_upgrade.click()
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -23,5 +30,10 @@ time.sleep(5)
 
 cookie = driver.find_element(By.XPATH, '//*[@id="bigCookie"]')
 
+next_check = time.time() + CHECK_INTERVAL
 while True:
     cookie.click()
+
+    if time.time() >= next_check:
+        check_for_upgrades()
+        next_check += CHECK_INTERVAL
