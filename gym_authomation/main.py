14c1bc02353a674e160ae221fcddcf0279ba8e05
Twoot <inenquser@gmail.com>
2026-04-28 16:49:00 +0300

    Add retry function with exceptions to handle network issues

--- gym_authomation/main.py
+++ gym_authomation/main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,35 +10,48 @@ GYM_URL = "https://appbrewery.github.io/gym/"
 ACCOUNT_EMAIL = "daniil@test.ru"
 ACCOUNT_PASSWORD = "g45yg45g45g34g4efdhgaJHGV"
 
+def retry(func, retries=7, description=None):
+    for i in range(retries):
+        print(f"Trying {description}. Attempt: {i + 1}")
+        try:
+            return func()
+        except TimeoutException:
+            if i == retries - 1:
+                raise
+            time.sleep(1)
+
+def login():
+    login_btn = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#login-button"))
+    )
+    login_btn.click()
+
+    email_input = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#email-input"))
+    )
+    email_input.clear()
+    email_input.send_keys(ACCOUNT_EMAIL)
+
+    password_input = driver.find_element(By.CSS_SELECTOR, "#password-input")
+    password_input.clear()
+    password_input.send_keys(ACCOUNT_PASSWORD)
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "#submit-button")
+    login_button.click()
+
+    wait.until(EC.presence_of_element_located((By.ID, "schedule-page")))
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 driver = webdriver.Chrome(options=chrome_options)
-
-wait = WebDriverWait(driver, 10)
-
 driver.get(GYM_URL)
 
-login = wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "#login-button"))
-)
-login.click()
+wait = WebDriverWait(driver, 2)
 
-email_input = wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "#email-input"))
-)
-email_input.send_keys(ACCOUNT_EMAIL)
-
-password_input = driver.find_element(By.CSS_SELECTOR, "#password-input")
-password_input.send_keys(ACCOUNT_PASSWORD)
-
-login_button = driver.find_element(By.CSS_SELECTOR, "#submit-button")
-login_button.click()
-
-wait.until(EC.presence_of_element_located((By.ID, "schedule-page")))
-
+retry(login, description="login")
 # ---------------------------- FIND AND BOOK CLASSES ---------------------------------- #
 booked_count = 0
 waitlist_count = 0
