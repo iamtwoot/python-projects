2da82e034debcd33e91d5d7521d1b2ebc0e70ef3
Twoot <inenquser@gmail.com>
2026-04-29 14:24:53 +0300

    Add Zillow scraper with listings parsing and data formatting

---
+++ capstone_automation/data_scraper.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+from bs4 import BeautifulSoup
+import requests
+
+
+def format_price(price: str):
+    return price.split("/")[0].replace("+", "").replace("1 bd", "").strip()
+
+
+class DataScraper:
+
+    def __init__(self):
+        self.data = {}
+        html = requests.get('https://appbrewery.github.io/Zillow-Clone/')
+        self.soup = BeautifulSoup(html.text, 'html.parser')
+
+    def get_listings(self):
+        return self.soup.select("div#grid-search-results > ul > li")
+
+    def get_data(self):
+        listings = self.get_listings()
+
+        for listing in listings:
+
+            listing_id = listings.index(listing) + 1
+
+            link_el = listing.select_one("a")
+            link = link_el.get("href") if link_el else None
+
+            price_el = listing.select_one("span[data-test='property-card-price']")
+            price = str(price_el.get_text(strip=True)) if price_el else None
+            formatted_price = format_price(price) if price else None
+
+            address_el = listing.select_one("address")
+            address = address_el.get_text(strip=True) if address_el else None
+
+            self.data[listing_id] = {
+                "link": link,
+                "price": formatted_price,
+                "address": address,
+            }
+
+        return self.data
+
+
+if __name__ == "__main__":
+    scraper = DataScraper()
+    scraper.get_data()
+    pprint(scraper.data)
+
---
+++ capstone_automation/main.py
@@ -0,0 +1,6 @@
+from data_scraper import DataScraper
+
+GOOGLE_FORM_LINK = "https://forms.gle/24THopc99YuMma4i9"
+
+scraper_bot = DataScraper()
+data = scraper_bot.get_data()
