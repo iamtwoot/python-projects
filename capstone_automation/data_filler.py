f7f492e4b1a3c65aa7ba223aee906145276e7472
Twoot <inenquser@gmail.com>
2026-04-29 14:32:07 +0300

    Init DataFiller class

---
+++ capstone_automation/data_filler.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+class DataFiller:
+
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+
+
