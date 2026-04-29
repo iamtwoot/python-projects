99fcdb796c362462cc8b83063fe0ea352e25a3ff
Twoot <inenquser@gmail.com>
2026-04-29 14:32:56 +0300

    Move form link

--- capstone_automation/data_filler.py
+++ capstone_automation/data_filler.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+GOOGLE_FORM_LINK = "https://forms.gle/24THopc99YuMma4i9"
+
 class DataFiller:
 
     def __init__(self):
--- capstone_automation/main.py
+++ capstone_automation/main.py
@@ -1,6 +1,4 @@
 from data_scraper import DataScraper
 
-GOOGLE_FORM_LINK = "https://forms.gle/24THopc99YuMma4i9"
-
 scraper_bot = DataScraper()
 data = scraper_bot.get_data()
