5c5458fd4c99fbe23f104aa5ace21bb697eb06bd
Twoot <inenquser@gmail.com>
2026-04-30 13:45:21 +0300

    Refactor blog posts into Post objects

--- blog_capstone_project/main.py
+++ blog_capstone_project/main.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
 
-posts_url = "https://api.npoint.io/c790b4d5cab58020d391"
-all_posts = requests.get(posts_url).json()
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = [Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts]
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", posts=post_objects)
 
-@app.route("/post/<int:post_id>")
-def show_post(post_id):
-    post = all_posts[post_id]
-    return render_template("post.html", post=post)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
