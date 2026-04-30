10c87c7ce0619f3a0a8b1a64041f9dfd69ed707f
Twoot <inenquser@gmail.com>
2026-04-30 16:54:55 +0300

    Add about and contact pages. Fix header template

--- blog_capstone_project/main.py
+++ blog_capstone_project/main.py
@@ -21,6 +21,14 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
