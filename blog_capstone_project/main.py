ef174ff544f06ad76a48fc1b9c097df8cbc7ad3a
Twoot <inenquser@gmail.com>
2026-04-30 13:33:18 +0300

    Make a link to a page with entire blog

--- blog_capstone_project/main.py
+++ blog_capstone_project/main.py
@@ -3,11 +3,18 @@ import requests
 
 app = Flask(__name__)
 
+posts_url = "https://api.npoint.io/c790b4d5cab58020d391"
+all_posts = requests.get(posts_url).json()
+
 @app.route('/')
 def home():
-    posts_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    all_posts = requests.get(posts_url).json()
     return render_template("index.html", posts=all_posts)
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    post = all_posts[post_id]
+    return render_template("post.html", post=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
