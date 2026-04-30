1409ac08c5c4e6227e70fa47cb20c50c04f82a54
Twoot <inenquser@gmail.com>
2026-04-30 17:02:31 +0300

    Remove Post class

--- blog_capstone_project/main.py
+++ blog_capstone_project/main.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template
 import requests
-from post import Post
 
 app = Flask(__name__)
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-post_objects = [Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts]
+posts = requests.get("https://api.npoint.io/029e2aed2baa7ed9f2cc").json()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts=post_objects)
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
