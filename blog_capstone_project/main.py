8f3ba0e1d26a572314cc0faf94a758d75b994080
Twoot <inenquser@gmail.com>
2026-05-01 11:56:26 +0300

    Send message using SMTP library

--- blog_capstone_project/main.py
+++ blog_capstone_project/main.py
@@ -1,9 +1,15 @@
+import os
+import smtplib
 from flask import Flask, render_template, request
 import requests
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
+EMAIL = os.getenv("MY_EMAIL")
+PASSWORD = os.getenv("MY_APP_PASSWORD")
 posts = requests.get("https://api.npoint.io/029e2aed2baa7ed9f2cc").json()
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -19,9 +25,11 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -30,7 +38,19 @@ def contact():
         email = request.form["email"]
         phone = request.form["phone"]
         message = request.form["message"]
-        print(username, email, phone, message)
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=EMAIL,
+                msg=f"Subject: New Message\n\n"
+                    f"Name: {username}\n"
+                    f"Email: {email}\n"
+                    f"Phone: {phone}\n"
+                    f"Message: {message}",
+            )
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html")
 
