864d12414a26c4f13362a21a99f1ec134e47e7bf
Twoot <inenquser@gmail.com>
2026-05-07 11:36:22 +0300

    Create database for new books

--- library_project/main.py
+++ library_project/main.py
@@ -1,29 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-all_books = []
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 @app.route('/')
 def home():
+    all_books = db.session.scalars(db.select(Book)).all()
+    print(all_books)
     return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_book = {
-            "title": request.form['title'],
-            "author": request.form['author'],
-            "rating": request.form['rating'],
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form['title'],
+            author=request.form['author'],
+            rating=float(request.form['rating']),
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
 
+    app.run(debug=True)
