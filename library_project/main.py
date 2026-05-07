aaf4cff374065a7039b6c5256528352888ee0495
Twoot <inenquser@gmail.com>
2026-05-07 12:13:28 +0300

    Create rating updating functionality

--- library_project/main.py
+++ library_project/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sdukjfg7es7g7gh34w7i6fg'
 
 
 class Base(DeclarativeBase):
@@ -45,9 +46,18 @@ def add():
         return redirect(url_for('home'))
     return render_template('add.html')
 
-@app.route("/edit_rating", methods=['GET', 'POST'])
-def edit_rating():
-    return render_template('edit_rating.html')
+@app.route("/edit/<book_id>", methods=['GET', 'POST'])
+def edit(book_id):
+    if request.method == 'POST':
+        new_rating = int(request.form['new_rating'])
+        book = db.get_or_404(Book, book_id)
+        book.rating = new_rating
+        db.session.commit()
+        flash('Rating updated successfully', 'success')
+        return redirect(url_for('home'))
+    else:
+        book = db.get_or_404(Book, book_id)
+        return render_template('edit_rating.html', book=book)
 
 if __name__ == "__main__":
     with app.app_context():
