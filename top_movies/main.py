7f098db6e29866cd3c90f277280f1858ce57df9a
Twoot <inenquser@gmail.com>
2026-05-12 11:38:04 +0300

    Update rating and review

--- top_movies/main.py
+++ top_movies/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
@@ -10,9 +10,12 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
+
 # CREATE DB
 class Base(DeclarativeBase):
     pass
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
@@ -31,6 +34,12 @@ class Movie(db.Model):
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
+# CREATE FORM
+class RateMovieForm(FlaskForm):
+    rating = StringField('Your Rating Out of 10', validators=[DataRequired()])
+    review = StringField('Your Review', validators=[DataRequired()])
+
+
 @app.route("/")
 def home():
     result = db.session.execute(db.select(Movie))
@@ -38,9 +47,26 @@ def home():
     return render_template("index.html", movies=all_movies)
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    form = RateMovieForm()
+    if form.validate_on_submit():
+        movie_id = request.args.get('id')
+        if movie_id is None:
+            abort(400)
+
+        movie_to_update = db.get_or_404(Movie, int(movie_id))
+
+        movie_to_update.rating = float(form.rating.data)
+        movie_to_update.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", form=form)
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-
 
     app.run(debug=True)
