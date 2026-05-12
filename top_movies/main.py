1f2e464c92710a1a441c5443dadf56c732604531
Twoot <inenquser@gmail.com>
2026-05-12 15:42:31 +0300

    Add movie ranking logic

--- top_movies/main.py
+++ top_movies/main.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template, redirect, url_for, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -46,7 +45,7 @@ class Movie(db.Model):
 
 # CREATE FORM
 class RateMovieForm(FlaskForm):
-    rating = StringField('Your Rating Out of 10', validators=[DataRequired()])
+    rating = StringField('Your Rating Out of 10')
     review = StringField('Your Review', validators=[DataRequired()])
 
 
@@ -70,8 +69,8 @@ def get_movies(title):
     return movies
 
 
-def get_movie_details(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+def get_movie_details(tmdb_id):
+    url = f"https://api.themoviedb.org/3/movie/{tmdb_id}"
     headers = {
         "accept": "application/json",
         "Authorization": f"Bearer {MOVIE_API_KEY}"
@@ -94,28 +93,39 @@ def add_movie(movie):
     db.session.commit()
     return new_movie
 
+
 @app.route("/")
 def home():
-    result = db.session.execute(db.select(Movie))
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating.desc()))
     all_movies = result.scalars().all()
+
+    for movie in all_movies:
+        movie.ranking = all_movies.index(movie) + 1
+
+    db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     form = RateMovieForm()
+
     if form.validate_on_submit():
         movie_id = request.args.get('id')
+
         if movie_id is None:
             abort(400)
 
         movie_to_update = db.get_or_404(Movie, int(movie_id))
 
-        movie_to_update.rating = float(form.rating.data)
-        movie_to_update.review = form.review.data
+        if form.rating.data:
+            movie_to_update.rating = float(form.rating.data)
+
+        if form.review.data:
+            movie_to_update.review = form.review.data
+
         db.session.commit()
         return redirect(url_for('home'))
-
     return render_template("edit.html", form=form)
 
 
