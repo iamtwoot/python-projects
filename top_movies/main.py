b75f4b757ebd4131a20090a210bb7dcffd445c6a
Twoot <inenquser@gmail.com>
2026-05-12 10:13:41 +0300

    Delete flask_bootstrap

--- top_movies/main.py
+++ top_movies/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
@@ -11,7 +10,6 @@ import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
 
 # CREATE DB
 
