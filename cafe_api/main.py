a6ede45d0e461674dd871199992c4c6748c1cd5f
Twoot <inenquser@gmail.com>
2026-05-13 13:01:08 +0300

    Create RESTful API

--- cafe_api/main.py
+++ cafe_api/main.py
@@ -1,5 +1,7 @@
+import random
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 
@@ -31,6 +33,12 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
 
 with app.app_context():
     db.create_all()
@@ -42,12 +50,77 @@ def home():
 
 
 # HTTP GET - Read Record
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+@app.route("/all")
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+@app.route("/search")
+def get_cafe_by_location():
+    query_location = request.args.get("loc")
+    result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
+    all_cafes = result.scalars().all()
+    if not all_cafes:
+        return jsonify(error={"Not Found" : "Sorry, we don`t have cafe at that location."}), 404
+    else:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
 # HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        seats=request.form.get("seats"),
+        has_toilet=bool(request.form.get("has_toilet")),
+        has_wifi=bool(request.form.get("has_wifi")),
+        has_sockets=bool(request.form.get("has_sockets")),
+        can_take_calls=bool(request.form.get("can_take_calls")),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
 # HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_coffee_price(cafe_id):
+    new_price = request.args.get("new_price")
+    try:
+        cafe = db.session.get(Cafe, cafe_id)
+    except AttributeError:
+        return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found."}), 404
+    else:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the coffee price."}), 200
+
 
 # HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api_key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.session.get(Cafe, cafe_id)
+        if cafe is None:
+            return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found."}), 404
+        else:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe."}), 200
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that`s not allowed. Make sure you have the correct api_key"}), 403
+
 
 
 if __name__ == '__main__':
